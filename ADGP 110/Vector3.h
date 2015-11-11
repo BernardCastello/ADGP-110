05be780a32d9a4744c5883fa313ea54c98668177
Bernard Castello <bernard.castello@yahoo.com>
2015-11-11 12:23:02 -0600

    ADGP 110

---
+++ ADGP 110/Vector3.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Vector3
+{
+	class Vector3Maths
+	{
+	public:
+
+		static float Add(float x, float y, float z);
+
+		static float Subtract(float x, float y, float z);
+
+		static float Multiply(float x, float y, float z);
+
+		static float Divide(float x, float y, float z);
+	};
+}
